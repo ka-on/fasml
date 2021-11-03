6effb986f23f0f365dd412118e29c0a496c1b1e4
Onur Kaya <89597147+ka-on@users.noreply.github.com>
2021-11-03 13:27:27 +0100

    Update input_gen.py

--- file_handling/input_gen.py
+++ file_handling/input_gen.py
@@ -1,8 +1,12 @@
-from file_handling.utils import shortest, occurring_features
-from networks.citrat_cycle.storage import CitrateStatic
+from file_handling.utils import shortest, occurring_features, randint
 import json
 import os
 import pandas as pd
+import numpy as np
+import tensorflow as tf
+import tensorflow_datasets as tfds
+import matplotlib.pyplot as plt
+
 
 
 def matrix_gen(proteome, pids, fids, regions):
@@ -45,6 +49,32 @@ def matrix_gen(proteome, pids, fids, regions):
     return df
 
 
+def matrix_gen_single(protein, fids, regions):
+    pa_pattern = []
+    for fid in fids:
+        tool = "coils2" if fid.split('_')[0] == "coils" else fid.split('_')[0]
+        for region in range(1, regions+1):
+            r_size = int(protein["length"] / region)
+            r_overhang = protein["length"] % region
+            r_start = 1
+            r_end = r_size+1 if r_overhang > 0 else r_size
+            for i in range(1, region):
+                r_start = r_end + 1
+                r_end = (r_end+r_size+1) if i < r_overhang else r_end+r_size
+            check = False
+            if fid in protein[tool]:
+                for instance in protein[tool][fid]['instance']:
+                    if (r_start <= instance[0] <= r_end) or (r_start <= instance[1] <= r_end):
+                        pa_pattern.append(1)
+                        check = True
+                        break
+                if not check:
+                    pa_pattern.append(0)
+            else:
+                pa_pattern.append(0)
+    return pa_pattern
+
+
 def regions_binary(data):
     pids = [protein for protein in data["feature"]]
     features = occurring_features(data)
@@ -78,3 +108,53 @@ def regions_binary_file_gen(path, outpath):
         output_file.write(output)
         output_file.close()
         i += 1
+
+
+def dataset_gen(path, annopath, group_name, root_dir):
+    """
+    TODO: This function could be broken down to smaller functions
+    """
+    file = open(os.path.join(path, group_name))
+    group_data = json.load(file)
+    # Positive data
+    px = [group_data["data"][protein] for protein in group_data["data"]]
+    py = [1 for i in range(0, len(px))]
+    # Generate file
+    outfile_path = os.path.join(root_dir,
+                                  "networks",
+                                  "citrate_cycle",
+                                  "data",
+                                  "datasets")
+    outfile_px_path = os.path.join(outfile_path, f"{group_name.split('.')[0]}_px.tsv")
+    outfile_py_path = os.path.join(outfile_path, f"{group_name.split('.')[0]}_py.tsv")
+    outfile_px = open(outfile_px_path, "a")
+    outfile_py = open(outfile_py_path, "a")
+    for j, pa_pattern in enumerate(px):
+        outfile_px.write('\t'.join([str(i) for i in pa_pattern])+'\n')
+        outfile_py.write(str(py[j])+'\t')
+    outfile_px.close()
+    outfile_py.close()
+
+    #Negative data
+    negative = len(px)*4
+    outfile_nx_path = os.path.join(outfile_path, f"{group_name.split('.')[0]}_nx.tsv")
+    outfile_ny_path = os.path.join(outfile_path, f"{group_name.split('.')[0]}_ny.tsv")
+    outfile_nx = open(outfile_nx_path, "a")
+    outfile_ny = open(outfile_ny_path, "a")
+    for i in range(0, negative):
+        if i % 20 == 0: print(f"Negative data {i} of {negative}")
+        random_file = os.listdir(annopath)[randint(len(os.listdir(path)))]
+        f = open(os.path.join(annopath, random_file))
+        negative_data = json.load(f)
+        f.close()
+        random_protein = [protein for protein in
+                          negative_data["feature"]][randint(len([protein for protein in negative_data["feature"]]))]
+        pa = matrix_gen_single(
+            negative_data["feature"][random_protein],
+            group_data["metadata"]["features"],
+            group_data["metadata"]["regions"])
+        outfile_nx.write('\t'.join([str(i) for i in pa]) + '\n')
+        outfile_ny.write('0\t')
+    outfile_nx.close()
+    outfile_ny.close()
+
