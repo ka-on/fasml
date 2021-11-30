30a23f80b38f7a418b62832b3e977234febc3a0b
julian <juliandosch@web.de>
2021-11-30 14:20:46 +0100

    fixed batching and log file

--- fasml/model_types/dense.py
+++ fasml/model_types/dense.py
@@ -44,17 +44,6 @@ class DenseLayersModel:
         if not os.path.isdir(group_weights_save_path):
             os.mkdir(group_weights_save_path)
 
-        # Log the training
-        log_dict = {}
-        if os.path.isfile(os.path.join(group_weights_save_path, "log.txt")):
-            i = 1
-            while os.path.isfile(os.path.join(group_weights_save_path, f"log_{i}.txt")):
-                i += 1
-            log_file = open(os.path.join(group_weights_save_path, f"log_{i}.txt"), 'a+')
-        else:
-            log_file = open(os.path.join(group_weights_save_path, "log.txt"), 'a+')
-
-
         cp_callback = tf.keras.callbacks.ModelCheckpoint(
             filepath=os.path.join(group_weights_save_path, self.name),
             save_weights_only=True,
@@ -62,42 +51,58 @@ class DenseLayersModel:
         )
         samples_fed = 0
         run = 1
+        batch_x = []
+        batch_y = []
+        print("Reading training data...", end = "\r")
         while samples_fed != px_length:
-            print(f"Run {run}/{int(px_length/32)}")
-
-            batch_size = min(32, px_length-samples_fed)
+            batch_size = min(10, px_length-samples_fed)
             batch_px = read_batch(px, batch_size)
             batch_nx = read_batch(nx, batch_size*4)  # The number multiplied has to be the same as the ratio 1:4
-            batch_x = batch_px + batch_nx
+            batch_x = batch_x + batch_px + batch_nx
             batch_py = [1 for i in batch_px]
             batch_ny = [0 for i in batch_nx]
 
-            batch_y = batch_py + batch_ny
+            batch_y = batch_y + batch_py + batch_ny
             samples_fed += batch_size
-            history = self.model.fit(
-                np.asarray(batch_x), np.asarray(batch_y),
-                callbacks=cp_callback,
-                epochs=epochv)
-            run += 1
-            #log_dict["runs"][run]["epochs"] = history.params["epochs"]
-            #log_dict["runs"][run]["steps"] = history.params["steps"]
-
+        print("Reading training data... Done!")
+        statistics = [len(batch_y)]
+        history = self.model.fit(
+            np.asarray(batch_x), np.asarray(batch_y),
+            callbacks=cp_callback,
+            epochs=epochv, batch_size=50)
         eval_samples_fed = 0
+        batch_x = []
+        batch_y = []
         while eval_samples_fed != eval_length:
-            batch_size = min(512, eval_length-eval_samples_fed)
+            batch_size = min(500, eval_length-eval_samples_fed)
             eval_samples_fed += batch_size
             print(eval_samples_fed)
             batch_px = read_batch(px, batch_size)
             batch_nx = read_batch(nx, batch_size*4)
-            batch_x = batch_px + batch_nx
+            batch_x = batch_x + batch_px + batch_nx
             batch_py = [1 for i in batch_px]
             batch_ny = [0 for i in batch_nx]
-            batch_y = batch_py + batch_ny
+            batch_y = batch_y + batch_py + batch_ny
             print("EVAL")
-            if batch_x:
-                self.model.evaluate(
-                    np.asarray(batch_x),
-                    np.asarray(batch_y))
+        statistics.append(len(batch_x))
+        scores = None
+        if batch_x:
+            scores = self.model.evaluate(
+                np.asarray(batch_x),
+                np.asarray(batch_y), 
+                batch_size=2500, 
+                verbose=1)
+        # Log the training
+        with open(os.path.join(group_weights_save_path, "log.txt"), 'w+') as log_file:
+            log_file.write("epochs: " + str(history.params["epochs"]) 
+                           + "\nsteps: " + str(history.params["steps"])
+                           + "\ntraining size: " + str(statistics[0])
+                           + "\neval size: " + str(statistics[1])
+                           + "\n\n# statistics per training epoch [loss & accuracy] #\n")
+            for i in range(len(history.history["loss"])):
+                log_file.write(f"epoch {str(i+1)}: {str(history.history['loss'][i]):.6} {str(history.history['accuracy'][i]):.6}\n")
+            if scores:
+                log_file.write(f"evaluation: {str(scores[0]):.6} {str(scores[1]):.6}")
         return history
 
 
